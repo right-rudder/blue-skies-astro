455abb513e60dac62ff4508c253955e93ce0cb04
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-07-15 05:49:55 -0300

    Update Enroll form

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly NEW_ENROLL_FORM_WEBHOOK_URL: string;
+  readonly PORTAL_API_KEY: string;
+  readonly CP_ACCOUNT_RANDOM_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
+}
