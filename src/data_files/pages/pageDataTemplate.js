d8628802aa3b88f1c2c43bc620852f272149b2ff
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-07-28 17:13:54 -0300

    Improve accessibility

--- src/data_files/pages/pageDataTemplate.js
+++ src/data_files/pages/pageDataTemplate.js
@@ -13,8 +13,9 @@ const homePage = {
       stars: true,
       imagePath: "/src/assets/images/.jpg",
       imageAlt: "",
-      headerH1: `Efficient training to become an <span class="text-picton-blue-500 dark:text-picton-blue-400">Airline Pilot</span> in Albert airline area`,
-      paragraph: "Flight school between Alabama and Georgia Nestled in the heart of Lanett, Alabama Lanett Municipal Airport 7A3",
+      headerH1: `Efficient training to become an <span class=" text-picton-blue-600 dark:text-picton-blue-400">Airline Pilot</span> in Albert airline area`,
+      paragraph:
+        "Flight school between Alabama and Georgia Nestled in the heart of Lanett, Alabama Lanett Municipal Airport 7A3",
       buttons: [
         {
           name: "Enroll Now",
@@ -32,13 +33,14 @@ const homePage = {
       partnersData: partnersData,
     },
 
-    featuresGeneral:{
-      title:"Preparing future pilots to launch their aviation careers",
-      subTitle:"Discover why aspiring pilots in Auburn and throughout the Alabama area trust Blue Skies Above to turn their aviation dreams into reality. With experienced instructors and comprehensive programs, we help students build the skills and confidence needed for a successful aviation career.",
-      imagePath:"",
-      imageAlt:"",
+    featuresGeneral: {
+      title: "Preparing future pilots to launch their aviation careers",
+      subTitle:
+        "Discover why aspiring pilots in Auburn and throughout the Alabama area trust Blue Skies Above to turn their aviation dreams into reality. With experienced instructors and comprehensive programs, we help students build the skills and confidence needed for a successful aviation career.",
+      imagePath: "",
+      imageAlt: "",
       features: features,
-    }
+    },
   },
 };
 export default homePage;
