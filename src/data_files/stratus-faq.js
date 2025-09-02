1f94598d8ce24598b3d7b6c3892dd47ec6942e3a
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-09-02 07:02:08 -0300

    Fix type error

---
+++ src/data_files/stratus-faq.js
@@ -0,0 +1,48 @@
+export const faqData = {
+  subTitle:
+    "At Blue Skies Above in Lanett, Alabama, we provide expert-led flight training for aspiring pilots across the Alabama–Georgia region.",
+  faqs: [
+    {
+      question: "What documents are required for the borrower?",
+      answer: `
+       <ul class="list-disc list-inside space-y-1 text-neutral-800 dark:text-neutral-400"> 
+        <li>Signed Private Student Loan Application (online link)</li>
+        <li>Signed Credit Report Authorization Form (online link combined above)</li>
+        <li>Signed Loan Application and Solicitation Disclosure Form (online link combined above)</li>
+        <li>Copy of Driver’s License</li>
+        <li>Copy of Passport or US Citizenship or Proof of Residency</li>
+        <li>Bank Statement for the past 60 days</li>
+        <li>2 months of pay stubs</li>
+       </ul>
+      `,
+    },
+    {
+      question:
+        "What documents are required for a co-borrower (if applicable)?",
+      answer: `
+       <ul class="list-disc list-inside space-y-1 text-neutral-800 dark:text-neutral-400"> 
+        <li>Signed Loan Application (online link)</li>
+        <li>Signed Credit Report Authorization (online link combined above)</li>
+        <li>Signed Loan Application and Solicitation Disclosure Form (online link combined above)</li>
+        <li>Copy of Driver’s License</li>
+        <li>Copy of Passport or US Citizenship or Proof of Residency</li>
+        <li>Bank Statement for the past 60 days</li>
+        <li>2 months of pay stubs</li>
+        </ul>
+      `,
+    },
+    {
+      question: "What school-related documents are required?",
+      answer: "School Approval for Private Student Loan",
+    },
+    {
+      question: "What pilot-related documents are required?",
+      answer: `
+        <ul class="list-disc list-inside space-y-1 text-neutral-800 dark:text-neutral-400">
+        <li>Copy of Medical Certificate (First or Second Class Only)</li>
+        <li>Student or Current Pilot’s Certificate</li>
+        </ul>
+      `,
+    },
+  ],
+};
