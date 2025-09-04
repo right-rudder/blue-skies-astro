6e6f12575cf4c0c38b22244b4cf6884249ea8df5
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-09-04 15:25:23 -0300

    Update SEO

---
+++ src/data_files/pages/lifestyle-aviation-page.js
@@ -0,0 +1,7 @@
+export const LifeStyleAviationPage = {
+  pageTitle: "LifeStyle Aviation | Blue Skies Above Flight School",
+  pageDescription:
+    "Learn how LifeStyle Aviation helps Blue Skies Above students and flight schools access modern Diamond aircraft through innovative programs, shared ownership, leaseback options, and tailored financing solutions.",
+  pageKeywords:
+    "LifeStyle Aviation, DiamondShare, Diamond Aircraft, flight school aircraft, aircraft financing, leaseback program, shared ownership, Blue Skies Above partners",
+};
